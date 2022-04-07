b31a42e655ac22396cb4db6c305cc3e43e43ec7b
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-07 21:45:47 +0300

    add completion date to the goals

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,12 +9,12 @@ import { Goal } from '../goal';
 export class GoalComponent implements OnInit {
 
   goals:Goal[]=[
-    new Goal(1,'Watch Dr.Robot','Find a  new season to keep me entertained'),
-    new Goal(2,'Bake cookies','Shop for all the required ingredients'),
-    new Goal(3,'Visit the mall','Enjoy coffee at my fav spot'),
-    new Goal(4,'Get a puppy','Go to the shelter to check out some options'),
-    new Goal(5,'Complete Angular IP','One commit at a time'),
-    new Goal(6,'Set the world on fire','Because girls run the world'),
+    new Goal(1,'Watch Dr.Robot','Find a  new season to keep me entertained',new Date(2020,3,14)),
+    new Goal(2,'Bake cookies','Shop for all the required ingredients',new Date(2019,6,9)),
+    new Goal(3,'Visit the mall','Enjoy coffee at my fav spot',new Date(2022,1,12)),
+    new Goal(4,'Get a puppy','Go to the shelter to check out some options',new Date(2019,0,18)),
+    new Goal(5,'Complete Angular IP','One commit at a time',new Date(2019,2,14)),
+    new Goal(6,'Set the world on fire','Because girls run the world',new Date(2030,3,14)),
   ];
 
   completeGoal(isComplete, index){
@@ -22,7 +22,7 @@ export class GoalComponent implements OnInit {
       this.goals.splice(index,1);
     }
   }
-  
+
   toggleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
