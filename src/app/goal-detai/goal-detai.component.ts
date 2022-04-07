18aa533273b28e0c7dd820e0ba32c996b9210498
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-07 22:30:38 +0300

    add delete button

--- src/app/goal-detai/goal-detai.component.ts
+++ src/app/goal-detai/goal-detai.component.ts
@@ -11,14 +11,14 @@ export class GoalDetaiComponent implements OnInit {
   @Input() goal: Goal;
   @Output() isComplete = new EventEmitter<boolean>();
 
-  goalComplete(complete:boolean){
-    this.isComplete.emit(complete);
-  }
+  // goalComplete(complete:boolean){
+  //   this.isComplete.emit(complete);
+  // }
 
   goalDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
-  
+
    constructor() { }
 
   ngOnInit(): void {
