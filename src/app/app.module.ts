d2f690a42e2d0328b94a43151c3bfbaf65879ffe
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-07 22:00:05 +0300

    create a pipe that will count the number of days left to the completion of a goal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
 import { GoalDetaiComponent } from './goal-detai/goal-detai.component';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     GoalComponent,
     GoalDetaiComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
