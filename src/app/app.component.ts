b98b740d900574a126c7951065b88794036cd35e
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-07 14:49:26 +0300

    add goals using blueprint class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goals:string[];
+  goals:Goal[]=[
+    {id:1,name:'Watch Dr.Robot'},
+    {id:2,name:'Bake cookies'},
+    {id:3,name:'Visit the mall'},
+    {id:4,name:'Get a puppy'},
+    {id:5,name:'Complete Angular IP'},
+    {id:6,name:'Set the world on fire'},
+  ];
 
-  constructor(){
-  this.goals = ['Watch Dr.Robot', 'Bake cookies','Visit the mall']
-}
+//   constructor(){
+//   this.goals = ['Watch Dr.Robot', 'Bake cookies','Visit the mall']
+// }
 
 }
--- src/app/goal.ts
+++ src/app/goal.ts
@@ -2,3 +2,4 @@ export class Goal {
   id: number;
   name: string;
 }
+
